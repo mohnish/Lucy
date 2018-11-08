230fc189c72655de745d5436971469c8a4d94bdc
Mohnish Thallavajhula <hi@iam.mt>
2018-11-08 12:00:48 -0800

    handle clearing text upon hitting esc

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 window.onload = function() {
   // By default, the bar is hidden
   const powerBar = document.querySelector('#lucy-power-bar');
+  const powerBarInput = powerBar.querySelector('input');
 
   function hide() {
   	powerBar.classList.add('hide');
@@ -14,26 +15,30 @@ window.onload = function() {
   	powerBar.classList.toggle('hide');
   }
 
+  function clear() {
+    powerBarInput.value = '';
+  }
+
   function get() {
-  	var input = powerBar.querySelector('input');
-  	return input.value;
+    return powerBarInput.value;
   }
 
   Mousetrap.bind('shift+l', function(e) {
   	e.preventDefault();
+
+    clear();
     toggle();
 
-    var input = powerBar.querySelector('input');
-  	input.focus();
+    powerBarInput.focus();
   });
 
   document.onkeydown = function(e) {
     var key = (e) ? e.which : window.event.keyCode;
     const command = get();
-    
+
     // 13 will handle enter and 27 will handle esc
-    if((key === 13 && command.length > 0) || key == 27) {
-    		hide();
+    if((key === 13 && command.length > 0) || key === 27) {
+      hide();
     }
   };
 };
