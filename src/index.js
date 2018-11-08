54bf629b10bbda1a8936a11a3d6308e5773be532
rajesh <rajesh@askmybraintrust.com>
2018-11-07 16:25:04 -0800

    mousetrap integrated

--- src/index.js
+++ src/index.js
@@ -1,19 +1,5 @@
 window.onload = function() {
-  document.getElementById("input-search").focus();
-  
-  document.onkeydown = function(e) {
-    var key = (e) ? e.which : window.event.keyCode;
-    console.log('key ', key);
-
-    if(key == 13) {
-    	const command = get();
-
-    	//pass to compromise and close
-    	if (command.length > 0) {
-    		close();
-    	}
-    }
-  };
+	document.getElementById("power-search").style.display = 'none';	 
 
   function close() {
   	var powerbar = document.getElementById('power-search');
@@ -24,4 +10,25 @@ window.onload = function() {
   	var input = document.getElementById('input-search');
   	return input.value;
   }
+
+  Mousetrap.bind('shift+l', function() {
+  	document.getElementById("power-search").style.display = '';
+  	var input = document.getElementById("input-search");
+
+  	input.focus();
+  	input.value = '';
+  });
+
+  document.onkeydown = function(e) {
+    var key = (e) ? e.which : window.event.keyCode;
+
+    if(key == 13) {
+    	const command = get();
+
+    	//pass to compromise and close
+    	if (command.length > 0) {
+    		close();
+    	}
+    }
+  };
 };
