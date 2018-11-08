82eeda5cda0113f7a0ecb35e5374f98b4e9e4079
Kathy Luo <kluo@marketo.com>
2018-11-08 15:25:51 -0800

    queries

--- src/mapping/simple.js
+++ src/mapping/simple.js
@@ -1,30 +1,36 @@
-import { withTags, allTags } from '~/src/compromise/parse';
+// import { withTags, allTags } from '~/src/compromise/parse';
 
 const sendEmail = (...params) => (console.log('Email', JSON.stringify(params)));
 const sendInvite = (...params) => (console.log('Invite', JSON.stringify(params)));
 const callPhone = (...params) => (console.log('Phone', JSON.stringify(params)));
-const createTask = (...params) => (console.log('Phone', JSON.stringify(params)));
+const createTask = (...params) => (console.log('Task', JSON.stringify(params)));
+const moveTo = (...params) => (console.log('Move To', JSON.stringify(params)));
 
 const sampleMapping = {
-  'send? email #Conjunction? #Email?': sendEmail,
-  'send? invite #Conjunction? #Email?': sendInvite,
-  'call? #PhoneNumber': callPhone,
-  'create? task': createTask,
+  '[#verb] * task #Preposition [(email|call|inmail|custom)]': createTask,
+  'send? email #Conjunction? [#FirstName] [#LastName]': sendEmail,
+  'send? email #Conjunction? [(#Email|#Person)]': sendEmail,
+  'send? invite #Conjunction? [(#Email|#Person)]': sendInvite,
+  'call? [#PhoneNumber]': callPhone,
+  'call [#NamedEntiity]': callPhone,
+  '(open|navigate|move) #Preposition? [*]': moveTo,
 }
 
 const findCallback = (input, mapping) => {
   let callback;
   Object.keys(mapping).some( (key) => {
     const parse = withTags(input, key);
-    if (parse != undefined || parse.length === 0) {
+    const valid = (parse != undefined && Object.keys(parse).length !== 0);
+
+    if (valid) {
       let params = allTags(input);
       callback = mapping[key].bind(null, parse, params);
     }
-    return (parse != undefined || parse.length === 0);
+    return valid;
   });
 
   if (!callback) {
-    return new Error('No match for input');
+    throw new Error('No match for input');
   }
 
   return callback;
