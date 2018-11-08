b77451102c1c814f4e577fbb6e5312c2c70a07f3
rajesh <rajesh@askmybraintrust.com>
2018-11-07 16:37:58 -0800

    integrated

--- src/index.js
+++ src/index.js
@@ -1,27 +1,30 @@
 window.onload = function() {
-	document.getElementById("power-search").style.display = 'none';	 
+	document.getElementById("lucy-power-bar").style.display = 'none';	 
 
   function close() {
-  	var powerbar = document.getElementById('power-search');
+  	var powerbar = document.getElementById('lucy-power-bar');
   	powerbar.style.display = 'none';
   }
 
   function get() {
-  	var input = document.getElementById('input-search');
+  	var input = document.getElementById('lucy-power-bar-input');
   	return input.value;
   }
 
   Mousetrap.bind('shift+l', function() {
-  	document.getElementById("power-search").style.display = '';
-  	var input = document.getElementById("input-search");
+  	console.log('key shift l');
+    document.getElementById("lucy-power-bar").style.display = '';
+  	var input = document.getElementById("lucy-power-bar-input");
 
   	input.focus();
   	input.value = '';
+    console.log('key shift 2');
   });
 
   document.onkeydown = function(e) {
+    
     var key = (e) ? e.which : window.event.keyCode;
-
+    console.log('key ', key);
     if(key == 13) {
     	const command = get();
 
